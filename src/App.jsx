a314e5262720c46a3a929736523c723ec94b9835
nimu725 <nimu725@gmail.com>
2025-07-24 12:06:52 +0530

    commit 8

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React <br></br>Y23 S119A are Working on Git Hub Actions</h1>
       <h1>Y23 S119A are Working on Git Hub Actions</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
